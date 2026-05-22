362ff7a6ccbfe4a22d97075a3341c76ad7bf8c5e
gambu763 <Gmemela1@gmail.com>
2026-05-22 13:04:58 +0200

    fixed s

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
-import Projects from "./Pages/Projects";s
+import Projects from "./Pages/Projects";
 
 export default function App() {
   return (
